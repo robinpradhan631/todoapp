d2e65b238479dcc7ded6df2ee53da46b908ba842
robinpradhan631@gmail.com <robinpradhan631@gmail.com>
2022-06-21 22:17:57 +0545

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <todo-app></todo-app>
+</template>
+<script>
+
+import TodoApp from './components/Todorun.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoApp
+  }
+}
+</script>
+
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
+import '../node_modules/@fortawesome/fontawesome-free/js/all.js'
+
+createApp(App).mount('#app')
